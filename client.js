cbda675899495ff48185525681498e863a46823d
roughbits01 <marwen.azouzi@gmail.com>
2015-11-28 15:50:32 +0100

    removing some bugs

--- client.js
+++ client.js
@@ -19,7 +19,6 @@ socket.on("areYouReady", function (data) {
   socket.emit("readyToPlay", {});
 });
 
-
 socket.on("playOption", function(data){
   $("#playOption").html(data.message);
   if (data.value) {
@@ -30,27 +29,16 @@ socket.on("playOption", function(data){
   }
 });
 
-socket.on("showRequestCardDialog", function(data) {
+/*socket.on("showRequestCardDialog", function(data) {
   if (data.option == "suite") {
     $("#suiteRequest").show();
   }
-});
+});*/
 
 function playCard(key, value) {
   index = key;
   playedCard = value;
-  /*if (parseInt(value) === 1) {
-    console.log("request card");
-    $("#suiteRequest").show();
-    $("#suiteRequestBtn").click(function() {
-      var request = $("#suiteRequestTxt").val();
-      socket.emit("suiteRequest", {tableID: 1, request: request});
-      socket.emit("playCard", {tableID:1, playedCard: playedCard, index: index});
-      console.log("called with request ==> " + request);
-    });
-  } else {*/
-    socket.emit("playCard", {tableID:1, playedCard: playedCard, index: index});
-  //}
+  socket.emit("playCard", {playedCard: playedCard, index: index});
 }
 
 socket.on("play", function(data) {
@@ -97,7 +85,7 @@ socket.on("turn", function(data) {
   } else {
     if(data.myturn) {
       $("#progressUpdate").html("<span class='label label-important'>It's your turn.</span>");
-      socket.emit("preliminaryRoundCheck", {tableID: 1}); //When a player has a turn, we need to control a few items, this is what enables us to make it happen.
+      socket.emit("preliminaryRoundCheck", {}); //When a player has a turn, we need to control a few items, this is what enables us to make it happen.
     } else {
       $("#progressUpdate").html("<span class='label label-info'>It's your opponent's turn.</span>");
     }
@@ -134,17 +122,13 @@ $(document).ready(function() {
     event.preventDefault();
   });
 
-$("#suiteRequestBtn").click(function() {
+/*$("#suiteRequestBtn").click(function() {
   var request = $("#suiteRequestTxt").val();
-  socket.emit("suiteRequest", {tableID: 1, request: request});
+  socket.emit("suiteRequest", {request: request});
   console.log("called with request ==> " + request);
-  //socket.emit("suiteRequest", {request: request, tableID: 1});
-  //socket.emit("playCard", {tableID:1, playedCard: playedCard, index: index});
-});
+});*/
 
 $("#create").click(function() {
-  //var privateTable = false;
-  //if ($("#key").is(':checked')) privateTable = true;
   var name = $("#createTableName").val();
   var count = $("#count").val();
   socket.emit("createTable", {name:name, playerLimit:count});
@@ -174,11 +158,12 @@ $("#join").click(function() {
   });
 
   $("#drawCard").click(function() {
-    socket.emit("drawCard", {tableID: 1});
+    socket.emit("drawCard", {});
   });
+
   /*penalising card taken button*/
   $("#penalising").click(function() {
-    socket.emit("penalisingTaken", {tableID: 1});
+    socket.emit("penalisingTaken", {});
     $("#penalising").hide();
   });
 
